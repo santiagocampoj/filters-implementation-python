09b496667c9fcd11e43140f04c5ca9926b191d2c
Santiago Campo <id00706137@usal.es>
2023-11-16 20:39:36 +0100

    Adding spectrogram plot analysis

--- low_pass_filter.py
+++ low_pass_filter.py
@@ -3,13 +3,14 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import sounddevice as sd
 import time
+from scipy.fft import fft, fftfreq
 
-# Setting up the constants for the low pass filter,
 # the cutoff frequency is 0.5 times the Nyquist frequency, which is 500 Hz
 CUTOFF_FREQ = 0.5
 
 # Creating the low-pass filter
 cutoff_frequency = CUTOFF_FREQ
+# The order refers to the complexity of the filter
 order = 6
 b, a = signal.butter(order, cutoff_frequency, 'low', analog=False)
 
@@ -19,41 +20,86 @@ fs = 1000
 t = np.linspace(0, 1, fs, endpoint=False)
 
 # Creating the input signal with low and high frequency components
-input_signal = np.sin(2 * np.pi * 5 * t) + np.sin(2 * np.pi * 50 * t)
+input_signal = np.sin(2 * np.pi * 5 * t) + np.sin(2 * np.pi * 100 * t) + np.sin(2 * np.pi * 200 * t) + np.sin(2 * np.pi * 400 * t)
+
+# converting input signal to stereo
+# input_signal = np.vstack((input_signal, input_signal)).T
 
 # Filtering the signal
 filtered_signal = signal.lfilter(b, a, input_signal)
 
-plt.figure()
-plt.plot(t, input_signal, label='Original Signal')
-plt.plot(t, filtered_signal, label='Filtered Signal')
-plt.xlabel('Time [seconds]')
-plt.ylabel('Amplitude')
-plt.title(f'Low Pass Filter with cutoff frequency {CUTOFF_FREQ}')
-plt.legend()
-plt.savefig(f'low_pass_filter_{CUTOFF_FREQ}.png')
-# plt.show()
-
-# Play the original signal
+# Play the original and filtered signals
 sd.play(input_signal, fs)
-sd.wait()  # Wait until the sound is played
-
-time.sleep(1)  # Wait for 1 second
-
-# Play the filtered signal
+sd.wait()
+time.sleep(1)
 sd.play(filtered_signal, fs)
-sd.wait()  # Wait until the sound is played
+sd.wait()
 
-# plot the frequency response of the filter
+
+# Plotting the signals and frequency spectrum
+# Start of the subplot plotting
+fig, axs = plt.subplots(3, 1, figsize=(10, 12))
+
+# Plotting the time-domain signals
+axs[0].plot(t, input_signal, label='Original Signal')
+axs[0].plot(t, filtered_signal, label='Filtered Signal')
+axs[0].set_title('Time Domain')
+axs[0].set_xlabel('Time [seconds]')
+axs[0].set_ylabel('Amplitude')
+axs[0].legend()
+
+# Plotting the frequency response
 w, h = signal.freqz(b, a)
-# freqz() is a function in scipy.signal that computes the frequency response of a filter
-plt.figure()
-plt.plot(w, 20 * np.log10(abs(h)))
+axs[1].plot(w, 20 * np.log10(abs(h)))
+axs[1].set_title('Frequency Response')
+axs[1].set_xlabel('Frequency [radians / second]')
+axs[1].set_ylabel('Amplitude [dB]')
+axs[1].grid(which='both', axis='both')
 
-plt.title('Butterworth filter frequency response')
-plt.xlabel('Frequency [radians / second]')
-plt.ylabel('Amplitude [dB]')
-plt.margins(0, 0.1)
-plt.grid(which='both', axis='both')
-plt.savefig(f'low_pass_filter_{CUTOFF_FREQ}_freq_response.png')
+# Plotting the frequency spectrum
+input_fft = fft(input_signal)
+filtered_fft = fft(filtered_signal)
+freqs = fftfreq(fs, 1/fs)
+axs[2].plot(freqs, np.abs(input_fft), label='Input Signal')
+axs[2].plot(freqs, np.abs(filtered_fft), label='Filtered Signal')
+axs[2].set_title('Frequency Spectrum')
+axs[2].set_xlabel('Frequency (Hz)')
+axs[2].set_ylabel('Amplitude')
+axs[2].set_xlim(0, fs/2)
+axs[2].legend()
+
+# Adjust layout
+plt.tight_layout()
+plt.savefig(f'low_pass_filter_freq_resp_{CUTOFF_FREQ}.png')
+plt.show()
+
+
+# Plotting the spectrogram
+# Creating a figure with two subplots
+fig, axs = plt.subplots(2, 1, figsize=(10, 8))
+
+# Compute and plot the spectrogram for the original signal
+f, t, Sxx = signal.spectrogram(input_signal, fs)
+pcm = axs[0].pcolormesh(t, f, 10 * np.log10(Sxx), shading='gouraud', cmap='inferno')
+axs[0].set_ylabel('Frequency [Hz]')
+axs[0].set_title('Spectrogram of Original Signal')
+
+# Compute and plot the spectrogram for the filtered signal
+f, t, Sxx = signal.spectrogram(filtered_signal, fs)
+axs[1].pcolormesh(t, f, 10 * np.log10(Sxx), shading='gouraud', cmap='inferno')
+axs[1].set_ylabel('Frequency [Hz]')
+axs[1].set_xlabel('Time [sec]')
+axs[1].set_title('Spectrogram of Filtered Signal')
+
+# Define a new axes for the colorbar
+# The values are [left, bottom, width, height] in figure coordinate space
+cbar_ax = fig.add_axes([0.92, 0.15, 0.02, 0.7]) # Adjust these values as needed
+
+# Adding colorbar to the new axes
+fig.colorbar(pcm, cax=cbar_ax, label='Magnitude (dB)')
+
+# Adjust layout
+plt.tight_layout()
+plt.subplots_adjust(right=0.9)  # Make room for the new colorbar
+plt.savefig(f'low_pass_filter_pectrogram_{CUTOFF_FREQ}.png')
 plt.show()
